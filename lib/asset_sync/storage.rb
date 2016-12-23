14f19784c9f6df93b5742b63c45da2fef4070fa9
Dylan Wang <wanghaoyu@frazil.me>
2016-12-23 21:37:16 +0800

    fix syntax issue

--- lib/asset_sync/storage.rb
+++ lib/asset_sync/storage.rb
@@ -127,7 +127,7 @@ module AssetSync
       uncompressed_filename = f.sub(/\.gz\z/, '')
       basename = File.basename(uncompressed_filename, File.extname(uncompressed_filename))
       if /-[0-9a-fA-F]{32}$/.match(basename) || # rails generated hash
-+        /^[0-9]+\.[0-9a-fA-F]{20}/.match(basename) # webpack generated chunk hash
+         /^[0-9]+\.[0-9a-fA-F]{20}/.match(basename) # webpack generated chunk hash
         file.merge!({
           :cache_control => "public, max-age=#{one_year}",
           :expires => CGI.rfc1123_date(Time.now + one_year)
